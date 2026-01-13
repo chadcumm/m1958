cc0817b30cde552a8629e042c4f8381eb8f1747a
chadcumm <algeron@gmail.com>
2026-01-13 15:00:26 -0600

    gbin-mha-pds: v0.0.46 | branch: develop | 2026-01-13T21-00-26

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -58,7 +58,7 @@ var routes = [
   },
   {
     path: "patients",
-    loadComponent: () => import("./chunk-VWGVPMNS.js").then((m) => m.PatientsComponent),
+    loadComponent: () => import("./chunk-X5MNBZFZ.js").then((m) => m.PatientsComponent),
     title: "MHA PDS Patients"
   }
 ];
@@ -93,8 +93,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.45-develop";
-var packageVersion = "0.0.45";
+var buildVersion = "v0.0.46-develop";
+var packageVersion = "0.0.46";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
