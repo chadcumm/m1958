a32cd23b45606071d86b5dbd47877c4fe74539f9
chadcumm <algeron@gmail.com>
2026-01-23 01:20:20 -0600

    gbin-mha-pds: v0.0.83 | branch: config-ui-update | 2026-01-23T07-20-20

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -51,7 +51,7 @@ var routes = [
   },
   {
     path: "config",
-    loadComponent: () => import("./chunk-M6WS2BQM.js").then((m) => m.ConfigEditorComponent),
+    loadComponent: () => import("./chunk-VKCZZBPE.js").then((m) => m.ConfigEditorComponent),
     title: "MHA PDS Configuration"
   },
   {
@@ -106,8 +106,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.82-config-ui-update";
-var packageVersion = "0.0.82";
+var buildVersion = "v0.0.83-config-ui-update";
+var packageVersion = "0.0.83";
 var gitBranch = "config-ui-update";
 
 // src/app/app-version/app-version.ts
