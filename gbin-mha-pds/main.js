2ea344acb4794ac5a72d24cabe348c6f0d2f0835
chadcumm <algeron@gmail.com>
2026-01-27 09:51:07 -0600

    gbin-mha-pds: v0.0.108 | branch: develop | 2026-01-27T15-51-07

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -106,8 +106,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.107-develop";
-var packageVersion = "0.0.107";
+var buildVersion = "v0.0.108-develop";
+var packageVersion = "0.0.108";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
