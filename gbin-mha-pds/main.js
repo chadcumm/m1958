bb2c81e988f97ead267d03d90c04ecfae3987b97
chadcumm <algeron@gmail.com>
2026-02-05 15:30:33 -0600

    gbin-mha-pds: v0.0.147 | branch: develop | 2026-02-05T21-30-33

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -56,7 +56,7 @@ var routes = [
   },
   {
     path: "logs",
-    loadComponent: () => import("./chunk-RKGDGQED.js").then((m) => m.LogsComponent),
+    loadComponent: () => import("./chunk-6G5YRZ3L.js").then((m) => m.LogsComponent),
     title: "MHA PDS Logs"
   },
   {
@@ -106,8 +106,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.146-develop";
-var packageVersion = "0.0.146";
+var buildVersion = "v0.0.147-develop";
+var packageVersion = "0.0.147";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
