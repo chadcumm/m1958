784f6aaccb195b5a2d69bc54373e747b992d0494
chadcumm <algeron@gmail.com>
2026-02-05 23:23:27 -0600

    gbin-mha-pds: v0.0.151 | branch: develop | 2026-02-06T05-23-27

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -51,7 +51,7 @@ var routes = [
   },
   {
     path: "config",
-    loadComponent: () => import("./chunk-S5PVGJ5N.js").then((m) => m.ConfigEditorComponent),
+    loadComponent: () => import("./chunk-TU6IT5OH.js").then((m) => m.ConfigEditorComponent),
     title: "MHA PDS Configuration"
   },
   {
@@ -106,8 +106,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.150-develop";
-var packageVersion = "0.0.150";
+var buildVersion = "v0.0.151-develop";
+var packageVersion = "0.0.151";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
