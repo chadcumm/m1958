7448893418feb6cdea4cef0e5e9fcc3669d00ba7
chadcumm <algeron@gmail.com>
2026-02-20 09:14:14 -0600

    gbin-mha-pds: v0.0.222 | branch: develop | 2026-02-20T15-14-14

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -93,7 +93,7 @@ var routes = [
   },
   {
     path: "audit",
-    loadComponent: () => import("./chunk-YSWWCWSU.js").then((m) => m.AuditComponent),
+    loadComponent: () => import("./chunk-3F2C52HW.js").then((m) => m.AuditComponent),
     title: "MHA PDS - Audit"
   },
   {
@@ -139,8 +139,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.221-develop";
-var packageVersion = "0.0.221";
+var buildVersion = "v0.0.222-develop";
+var packageVersion = "0.0.222";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
