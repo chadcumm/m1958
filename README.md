3533d731b5bf1f850411f2338027d0e0fd53bf5c
chadcumm <algeron@gmail.com>
2026-03-09 11:11:34 -0600

    gbin-mha-pds: v0.0.253 | branch: develop | 2026-03-09T17-11-34

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## Current Deployment
 
 **Environment:** M1958
-**Version:** v0.0.252
+**Version:** v0.0.253
 **Branch:** develop
-**Last Deployed:** 03/05/2026, 13:40:37
+**Last Deployed:** 03/09/2026, 11:11:34
 
 ## About
 
@@ -13,6 +13,7 @@ This directory contains the deployed build artifacts for the gbin-mha-pds MPage 
 
 ## Deployment History
 
+- **v0.0.253** | Branch: `develop` | Deployed: 03/09/2026, 11:11:34
 - **v0.0.252** | Branch: `develop` | Deployed: 03/05/2026, 13:40:37
 - **v0.0.251** | Branch: `develop` | Deployed: 03/05/2026, 10:00:18
 - **v0.0.250** | Branch: `develop` | Deployed: 03/05/2026, 08:29:17
@@ -22,7 +23,6 @@ This directory contains the deployed build artifacts for the gbin-mha-pds MPage 
 - **v0.0.246** | Branch: `develop` | Deployed: 02/26/2026, 11:43:31
 - **v0.0.246** | Branch: `develop` | Deployed: 02/26/2026, 11:43:29
 - **v0.0.245** | Branch: `develop` | Deployed: 02/25/2026, 20:52:41
-- **v0.0.245** | Branch: `develop` | Deployed: 02/25/2026, 20:52:39
 
 ---
 
--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -78,33 +78,33 @@ var routes = [
   },
   {
     path: "config",
-    loadComponent: () => import("./chunk-KAQA2T4P.js").then((m) => m.ConfigEditorComponent),
+    loadComponent: () => import("./chunk-BWDMHOKM.js").then((m) => m.ConfigEditorComponent),
     title: "MHA PDS Configuration"
   },
   {
     path: "logs",
-    loadComponent: () => import("./chunk-4ZSTHT7I.js").then((m) => m.LogsComponent),
+    loadComponent: () => import("./chunk-SXZUSV2K.js").then((m) => m.LogsComponent),
     title: "MHA PDS Logs"
   },
   {
     path: "patients",
-    loadComponent: () => import("./chunk-4ON4GHP4.js").then((m) => m.PatientsComponent),
+    loadComponent: () => import("./chunk-UIEXILEO.js").then((m) => m.PatientsComponent),
     title: "MHA PDS Patients"
   },
   {
     path: "audit",
-    loadComponent: () => import("./chunk-5PBOIMHS.js").then((m) => m.AuditComponent),
+    loadComponent: () => import("./chunk-BINOIP6L.js").then((m) => m.AuditComponent),
     title: "MHA PDS - Audit"
   },
   {
     path: "operations",
-    loadComponent: () => import("./chunk-YWVOBITH.js").then((m) => m.OperationsComponent),
+    loadComponent: () => import("./chunk-FKUT6BUA.js").then((m) => m.OperationsComponent),
     canDeactivate: [OperationsCanDeactivateGuard],
     title: "MHA PDS Operations"
   },
   {
     path: "reference",
-    loadComponent: () => import("./chunk-QY5EZVHV.js").then((m) => m.ReferenceComponent),
+    loadComponent: () => import("./chunk-UF6ND7AJ.js").then((m) => m.ReferenceComponent),
     title: "MHA PDS Reference"
   }
 ];
@@ -139,8 +139,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.252-develop";
-var packageVersion = "0.0.252";
+var buildVersion = "v0.0.253-develop";
+var packageVersion = "0.0.253";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
