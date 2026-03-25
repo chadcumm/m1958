a2167164aef142022a8cc9de369df839b292bf4a
chadcumm <algeron@gmail.com>
2026-03-25 15:23:12 -0500

    gbin-mha-pds: v0.0.286 | branch: develop | 2026-03-25T20-23-12

--- gbin-mha-pds/main.js
+++ gbin-mha-pds/main.js
@@ -145,7 +145,7 @@ var routes = [
   },
   {
     path: "reports",
-    loadComponent: () => import("./chunk-2ZLWVLLZ.js").then((m) => m.ReportsComponent),
+    loadComponent: () => import("./chunk-SZ7DJAXK.js").then((m) => m.ReportsComponent),
     canActivate: [tabAccessGuard],
     title: "MHA PDS Reports"
   },
@@ -186,8 +186,8 @@ var appConfig = {
 };
 
 // src/app/version.ts
-var buildVersion = "v0.0.285-develop";
-var packageVersion = "0.0.285";
+var buildVersion = "v0.0.286-develop";
+var packageVersion = "0.0.286";
 var gitBranch = "develop";
 
 // src/app/app-version/app-version.ts
